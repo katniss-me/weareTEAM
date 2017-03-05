1c5cb4b5d6cd31ecc07ac797ee5271740a840aed
HanYoungsoo <gglife333@gmail.com>
2017-03-05 19:17:00 +0900

    지도에 내 위치 표시하기 / 위치 정보 가져오기

--- AppMate/AppMate/ViewController.swift
+++ AppMate/AppMate/ViewController.swift
@@ -7,16 +7,59 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
+
+    let initialLocation = CLLocation(latitude: 37.3858128, longitude: 127.123164);
+    let regionRadius: CLLocationDistance = 1000
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locataion = locations[0]
+//        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+//        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(locataion.coordinate.latitude+10, locataion.coordinate.longitude+10)
+        let region:MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(
+            CLLocationCoordinate2DMake(locataion.coordinate.latitude+0.01, locataion.coordinate.longitude), 10000, 10000);
+        
+        mapView.setRegion(region, animated:true)
+        self.mapView.showsUserLocation = true
+        self.manager.stopUpdatingLocation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+        
+        
+//        centerMapOnLocation(location:initialLocation)
+    
     }
+    
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
+
     }
 
 
