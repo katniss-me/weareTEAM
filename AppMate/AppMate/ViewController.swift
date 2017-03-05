133ad065da45cc6fd20383a7ab39eed0f3a39660
HanYoungsoo <gglife333@gmail.com>
2017-03-05 16:57:38 +0900

    ios project commit

---
+++ AppMate/AppMate/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppMate
+//
+//  Created by HanYoungsoo on 2017. 3. 5..
+//  Copyright © 2017년 YoungsooHan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
