539f0227ea8e8f7c339b04554b54fd5a382d0530
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-03-05 22:51:54 +0900

    db name restAPI -> restapi

--- restAPI/restAPI/settings.py
+++ restAPI/restAPI/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'restAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'restAPI',
+        'NAME': 'restapi',
         'USER': 'postgres',
         'PASSWORD': '999999999',
         'HOST': 'localhost',
