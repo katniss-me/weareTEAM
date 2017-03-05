e47f8a43db72a1926295093c5a578c02c4b64940
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-03-06 01:31:27 +0900

    set owner readonly, add permission to locations

--- restAPI/location/serializers.py
+++ restAPI/location/serializers.py
@@ -2,12 +2,14 @@ from location.models import Appointed, Current
 from rest_framework import serializers
 
 class AppointedSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Appointed
         fields = ('id', 'owner', 'address', 'location')
 
 class CurrentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Current
--- restAPI/location/views.py
+++ restAPI/location/views.py
@@ -1,12 +1,16 @@
+from rest_framework import permissions
+
 from location.models import Appointed, Current
+from location.permission import IsOwnerOrReadOnly
 from location.serializers import AppointedSerializer, CurrentSerializer
 from rest_framework import generics
 
-# Create your views here.
 class AppointedLocationsView(generics.ListCreateAPIView):
     queryset = Appointed.objects.all()
     serializer_class = AppointedSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
 
 class CurrentLocationsView(generics.ListCreateAPIView):
     queryset = Current.objects.all()
     serializer_class = CurrentSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
